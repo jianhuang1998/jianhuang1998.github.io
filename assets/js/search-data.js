5d7a783b32e4cbe6e192f2b83096e22d761d5715
jianhuang1998 <jianhuang1998@gmail.com>
2026-05-19 02:20:52 +0000

    Deploying to gh-pages from @ jianhuang1998/jianhuang1998.github.io@2435d0bed2c242d482bb5509e45c0de46dcff763 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,22 +31,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
+          section: "News",},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
